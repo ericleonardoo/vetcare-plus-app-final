164233d064ab27852ace3db3630d080b40e3ca4e
Eric Leonardo <voidxff1@gmail.com>
2025-09-23 23:11:50 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -6,13 +7,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "studio-8857767906-b1d09",
-  appId: "1:435186047851:web:e768f398071646b9f10a2e",
-  storageBucket: "studio-8857767906-b1d09.appspot.com",
-  apiKey: "AIzaSyDVsKdG-Cl7iHRJfD8TqmBMnrPd-minoyM",
-  authDomain: "studio-8857767906-b1d09.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "435186047851",
+  "projectId": "studio-8857767906-b1d09",
+  "appId": "1:435186047851:web:e768f398071646b9f10a2e",
+  "storageBucket": "studio-8857767906-b1d09.appspot.com",
+  "apiKey": "AIzaSyDVsKdG-Cl7iHRJfD8TqmBMnrPd-minoyM",
+  "authDomain": "studio-8857767906-b1d09.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "435186047851"
 };
 
 // Initialize Firebase
