2cccc42706b66078dd83f78437fc66fe5fa4c26f
Eric Leonardo <voidxff1@gmail.com>
2025-09-24 16:48:34 +0000

    ainda não funcionou, nenhum usuário foi autenticado no firestore e també

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import { AuthProvider } from '@/context/AuthContext';
 export const metadata: Metadata = {
   title: 'VetCare+',
   description: 'Cuidado Excepcional para o Seu Melhor Amigo',
-  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -25,7 +24,6 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased font-normal">
         <AuthProvider>
